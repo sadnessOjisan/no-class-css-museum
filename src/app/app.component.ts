06932638e365d507382ca193064fc4ad53aef28c
sadness_ojisan <sadness.ojisan@gmail.com>
2020-07-28 06:10:12 +0900

    toggle css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // 	そのコンポーネントが他のHTMLから参照されるときのタグ名になる。
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-app';
