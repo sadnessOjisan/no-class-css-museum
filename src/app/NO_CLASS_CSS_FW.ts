45a19d3971cc47b53331aa54a8f849de021aaec4
sadness_ojisan <sadness.ojisan@gmail.com>
2020-07-29 06:38:50 +0900

    add all css

--- src/app/NO_CLASS_CSS_FW.ts
+++ src/app/NO_CLASS_CSS_FW.ts
@@ -8,7 +8,25 @@ export type CSS_FW_NAME_TYPE =
   | 'AttriCSS(Dark Fairy Pink)'
   | 'AttriCSS(Light Fairy Pink)'
   | 'milligram'
-  | 'new.css';
+  | 'new.css'
+  | 'awsm.css'
+  | 'Bahunya'
+  | 'holiday.css'
+  | 'markdowncss(splendor)'
+  | 'markdowncss(Retro)'
+  | 'marx'
+  | 'sakura'
+  | 'sakura(dark)'
+  | 'sakura(dark-solarized)'
+  | 'sakura(earthly)'
+  | 'sakura(ink)'
+  | 'sakura(vader)'
+  | 'style.css'
+  | 'Stylize.css'
+  | 'tufte-css'
+  | 'Tacit'
+  | 'Writ'
+  | 'bullframe.css';
 
 type NoClassCssFwType = {
   name: CSS_FW_NAME_TYPE;
@@ -74,6 +92,110 @@ const NO_CLASS_CSS_FWS: NoClassCssFwType[] = [
     name: 'new.css',
     officialURL: 'https://newcss.net/',
     downloadURL: 'https://github.com/xz/new.css/blob/master/new.css',
+  },
+  {
+    name: 'awsm.css',
+    officialURL: 'https://igoradamenko.github.io/awsm.css/index.html',
+    downloadURL:
+      'https://github.com/igoradamenko/awsm.css/blob/master/dist/awsm.min.css',
+  },
+  {
+    name: 'Bahunya',
+    officialURL: 'https://github.com/kimeiga/bahunya',
+    downloadURL: 'https://cdn.rawgit.com/kimeiga/bahunya/css/bahunya-0.1.3.css',
+  },
+  {
+    name: 'bullframe.css',
+    officialURL: 'https://github.com/marcop135/bullframe.css',
+    downloadURL:
+      'https://github.com/marcop135/bullframe.css/blob/master/dist/css/bullframe.min.css',
+  },
+  {
+    name: 'holiday.css',
+    officialURL: 'https://holidaycss.js.org/',
+    downloadURL:
+      'https://github.com/EvgenyOrekhov/holiday.css/blob/master/dist/holiday.css',
+  },
+  {
+    name: 'markdowncss(splendor)',
+    officialURL: 'https://markdowncss.github.io/',
+    downloadURL:
+      'https://github.com/markdowncss/splendor/blob/master/css/splendor.min.css',
+  },
+  {
+    name: 'markdowncss(Retro)',
+    officialURL: 'https://markdowncss.github.io/',
+    downloadURL:
+      'https://github.com/markdowncss/retro/blob/master/css/retro.css',
+  },
+  {
+    name: 'marx',
+    officialURL: 'https://mblode.github.io/marx/',
+    downloadURL: 'https://github.com/mblode/marx/blob/master/css/marx.min.css',
+  },
+  {
+    name: 'sakura',
+    officialURL: 'https://oxal.org/projects/sakura/demo/',
+    downloadURL: 'https://github.com/oxalorg/sakura/blob/master/css/sakura.css',
+  },
+  {
+    name: 'sakura(dark)',
+    officialURL: 'https://oxal.org/projects/sakura/demo/',
+    downloadURL:
+      'https://github.com/oxalorg/sakura/blob/master/css/sakura-dark.css',
+  },
+  {
+    name: 'sakura(dark-solarized)',
+    officialURL: 'https://oxal.org/projects/sakura/demo/',
+    downloadURL:
+      'https://github.com/oxalorg/sakura/blob/master/css/sakura-dark-solarized.css',
+  },
+  {
+    name: 'sakura(earthly)',
+    officialURL: 'https://oxal.org/projects/sakura/demo/',
+    downloadURL:
+      'https://github.com/oxalorg/sakura/blob/master/css/sakura-earthly.css',
+  },
+  {
+    name: 'sakura(ink)',
+    officialURL: 'https://oxal.org/projects/sakura/demo/',
+    downloadURL:
+      'https://github.com/oxalorg/sakura/blob/master/css/sakura-ink.css',
+  },
+  {
+    name: 'sakura(vader)',
+    officialURL: 'https://oxal.org/projects/sakura/demo/',
+    downloadURL:
+      'https://github.com/oxalorg/sakura/blob/master/css/sakura-vader.css',
+  },
+  {
+    name: 'style.css',
+    officialURL: 'https://css-pkg.github.io/style.css/',
+    downloadURL: 'https://unpkg.com/style.css@1.0.0/style.css',
+  },
+  {
+    name: 'Stylize.css',
+    officialURL: 'https://vasanthv.com/stylize.css/demo.html',
+    downloadURL:
+      'https://github.com/vasanthv/stylize.css/blob/master/stylize.min.css',
+  },
+  {
+    name: 'tufte-css',
+    officialURL: 'https://github.com/edwardtufte/tufte-css',
+    downloadURL:
+      'https://github.com/edwardtufte/tufte-css/blob/gh-pages/tufte.min.css',
+  },
+  {
+    name: 'Tacit',
+    officialURL: 'https://yegor256.github.io/tacit/',
+    downloadURL:
+      'https://cdn.jsdelivr.net/gh/yegor256/tacit@gh-pages/tacit-css.min.css',
+  },
+  {
+    name: 'Writ',
+    officialURL: 'https://writ.cmcenroe.me/',
+    downloadURL:
+      'https://github.com/causal-agent/writ/blob/master/writ.min.css',
   },
 ];
 
--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -109,3 +109,201 @@ export class AttriMidnightGreen implements OnInit {
 
   ngOnInit(): void {}
 }
+
+@Component({
+  selector: 'awsm',
+  templateUrl: './template.component.html',
+  styleUrls: ['./awsm.css'],
+})
+export class Awsm implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+@Component({
+  selector: 'bahunya',
+  templateUrl: './template.component.html',
+  styleUrls: ['./bahunya.css'],
+})
+export class Bahunya implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+@Component({
+  selector: 'bullframe',
+  templateUrl: './template.component.html',
+  styleUrls: ['./bullframe.min.css'],
+})
+export class Bullframe implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+@Component({
+  selector: 'holiday',
+  templateUrl: './template.component.html',
+  styleUrls: ['./holiday.css'],
+})
+export class Holiday implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+@Component({
+  selector: 'marx',
+  templateUrl: './template.component.html',
+  styleUrls: ['./marx.min.css'],
+})
+export class Marx implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+@Component({
+  selector: 'retro',
+  templateUrl: './template.component.html',
+  styleUrls: ['./retro.css'],
+})
+export class Retro implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+@Component({
+  selector: 'sakura',
+  templateUrl: './template.component.html',
+  styleUrls: ['./sakura.css'],
+})
+export class Sakura implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+@Component({
+  selector: 'sakura-dark',
+  templateUrl: './template.component.html',
+  styleUrls: ['./sakura-dark.css'],
+})
+export class SakuraDark implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+@Component({
+  selector: 'sakura-dark-solarized',
+  templateUrl: './template.component.html',
+  styleUrls: ['./sakura-dark-solarized.css'],
+})
+export class SakuraDarkSolarized implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+@Component({
+  selector: 'sakura-earthly',
+  templateUrl: './template.component.html',
+  styleUrls: ['./sakura-earthly.css'],
+})
+export class SakuraEarhly implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+@Component({
+  selector: 'sakura-ink',
+  templateUrl: './template.component.html',
+  styleUrls: ['./sakura-ink.css'],
+})
+export class SakuraInk implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+@Component({
+  selector: 'sakura-vader',
+  templateUrl: './template.component.html',
+  styleUrls: ['./sakura-vader.css'],
+})
+export class SakuraVader implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+@Component({
+  selector: 'splendor',
+  templateUrl: './template.component.html',
+  styleUrls: ['./splendor.min.css'],
+})
+export class Splendor implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+@Component({
+  selector: 'style-css',
+  templateUrl: './template.component.html',
+  styleUrls: ['./style.css'],
+})
+export class Style implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+@Component({
+  selector: 'stylize',
+  templateUrl: './template.component.html',
+  styleUrls: ['./stylize.min.css'],
+})
+export class Stylize implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+@Component({
+  selector: 'tacit-css',
+  templateUrl: './template.component.html',
+  styleUrls: ['./tacit-css.min.css'],
+})
+export class TacitCss implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+@Component({
+  selector: 'tufte-css',
+  templateUrl: './template.component.html',
+  styleUrls: ['./tufte.min.css'],
+})
+export class Tufte implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+@Component({
+  selector: 'writ',
+  templateUrl: './template.component.html',
+  styleUrls: ['./writ.min.css'],
+})
+export class Writ implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
